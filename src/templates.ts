0b7176c0613cb744733c6de051d9fc9920e219ac
Kyrylo Novikov <novikovkyrylo@gmail.com>
2026-03-24 12:10:59 +0100

    refactor(templates): fix layout structure in settings templates

--- src/templates.ts
+++ src/templates.ts
@@ -2,19 +2,19 @@ import { Setting } from "./interfaces";
 
 export function showSettingsTemplate() {
   return `<section class="settings-sec">
-                          <h1>Settings</h1>
+                          <h1 class="">Settings</h1>
                           <div class="setting-wrapper">
-                            <div id="settings-content"></div>
+                            <div id="settings-content" class="settings-content"></div>
                             <div class="choosen">
-                              <img class="img-theme" id="img-theme" src=""  alt="img of the theme">
+                              <img class="img-theme" id="img-theme" src="public/assets/imgs/game-theme/theme-placeholder.png"  alt="img of the theme">
                               <aside>
-                                <div id="show-theme" class="output-choose">Game theme</div>
-                                <span class="separator"></span>
-                                <div id="show-player"class="output-choose">Player</div>
-                                <span class="separator"></span>
-                                <div id="show-size" class="output-choose">Board size</div>
-                                <button id="initGameBtn" disabled >
-                                  <img src="" alt="">
+                                <div class="single-choose-wrapper">Theme<div id="show-theme" class="output-choose"></div></div>
+                                <span class="separator separator-unchecked"></span>
+                                <div class="single-choose-wrapper" id="show-player">Player<div class="output-choose"></div></div>
+                                <span class="separator separator-unchecked"></span>
+                                <div class="single-choose-wrapper">Board size <div id="show-size" class="output-choose"></div></div>
+                                <button class="btn __init-game" id="initGameBtn" disabled >
+                                  <img src="assets/icons/smart_display.svg" alt="">
                                   Start
                                 </button>
                               </aside>
@@ -28,16 +28,17 @@ export function createChooseTemplate(setting: Setting) {
   construcktSettings += `<div class="setting-choose-wrapper">
                                 <img src="${setting.icon}" alt="Show icon for ${setting.legend}" class="icon-setting" srcset=""/>
                                 <fieldset> 
-                                  <legend >
+                                  <legend>
                                   ${setting.legend}
-                                  </legend>`;
+                                  </legend><div class="option-wrapper">`;
   setting.option.forEach((opt) => {
-    construcktSettings += `   <div>
+    construcktSettings += `  <div> 
                                     <input name="${setting.type}" type="radio" value="${opt.value}" id="${opt.id}"/>
                                     <label for="${opt.id}">${opt.label}</label>
+                                    <div class="visual-for-checked"> </div>
                                   </div>`;
   });
-  construcktSettings += `</fieldset>
+  construcktSettings += `</div></fieldset>
                               </div>`;
   return construcktSettings;
 }
